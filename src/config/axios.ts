36496e512442045da3f639486285b0754bd6e2a2
Pickky03 <vhai02092003@gmail.com>
2025-09-05 18:20:56 +0700

    last fix3

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
-  withCredentials: true,
+  withCredentials: false,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
